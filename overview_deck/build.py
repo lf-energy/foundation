29d982f04679b3e098b42b62a0443b0a3c5a0804
John Mertic <jmertic@linuxfoundation.org>
2023-05-30 09:03:13 -0400

    No longer use Google Auth flow
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- overview_deck/build.py
+++ overview_deck/build.py
@@ -7,13 +7,7 @@
 
 from __future__ import print_function
 import os.path
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 import requests
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 # The ID of a sample document.
 documents = [
@@ -22,23 +16,6 @@ documents = [
         {'id':'1scoG9tuWM3mSRFTFgw5ObVFF6L06nYfztE2gKaFs1_8','mimeType':'application/pdf','filename':'LF Energy Membership Overview.pdf'},
         ]
 
-creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
 
 # Retrieve the documents contents from the Docs service.
 for document in documents:
