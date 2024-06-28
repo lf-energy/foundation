679dec14ccf6c033dbf7bac6f7486d87c2805c70
John Mertic <jmertic@linuxfoundation.org>
2024-06-28 16:44:40 -0400

    Update doc link for the overview deck
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- overview_deck/build.py
+++ overview_deck/build.py
@@ -11,8 +11,8 @@ import requests
 
 # The ID of a sample document.
 documents = [
-        {'id': '1c_cUR1wWgAFlckHFAbKCl0lQ5Bt9d_X54rkfTRDqvyA','mimeType':'application/pdf','filename':'LF Energy High Level Overview Deck.pdf'},
-        {'id': '1c_cUR1wWgAFlckHFAbKCl0lQ5Bt9d_X54rkfTRDqvyA','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'LF Energy High Level Overview Deck.pptx'},
+        {'id': '1DMZJd5dQBsrn1CtiuCCdfDftS0tsMRCq9JTSLHxb6UY','mimeType':'application/pdf','filename':'LF Energy High Level Overview Deck.pdf'},
+        {'id': '1DMZJd5dQBsrn1CtiuCCdfDftS0tsMRCq9JTSLHxb6UY','mimeType':'application/vnd.openxmlformats-officedocument.presentationml.presentation','filename':'LF Energy High Level Overview Deck.pptx'},
         {'id':'19Gysxwk8875Thb9YlBnP0Ub4pokgDIz7rmEsazjcwCk','mimeType':'application/pdf','filename':'LF Energy TAC Overview.pdf'},
         {'id':'1qsCjeTsv0CVie6HQfi1vHqyfpjmFojWqpXfuIbpRWWk','mimeType':'application/pdf','filename':'LF Energy Governing Board Overview.pdf'},
         {'id':'1scoG9tuWM3mSRFTFgw5ObVFF6L06nYfztE2gKaFs1_8','mimeType':'application/pdf','filename':'LF Energy Membership Overview.pdf'},
